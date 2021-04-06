753b56dc8b949fe02b39f569dbc9063380dbf661
gyunini <80234012+gyunini@users.noreply.github.com>
2021-04-06 20:20:30 +0900

    Add files via upload

--- Assignment01.cs
+++ Assignment01.cs
@@ -1,33 +1,27 @@
 using System;
-using doit_study_homework_template.testcase;
 using System.Collections.Generic;
+using DoitStudy.Interface;
+using DoitStudy.Testcase;
+using Assignment01Class;
 
-namespace doit_study_homework_template
+namespace DoitStudy.Assignments
 {
-    public class Assignment01 : TestCase01, IAssignment
+    public class Assignment01 : IAssignment
     {
-        /* Dont Touch */
-        public bool isRunning { get;  set; }
-        public Assignment01(bool _isRunning){
-            isRunning = _isRunning;
-        }
-        /********************/
-        public void main(){
-            var inputs = input();
-            var results = new List<int>();
-
-            foreach (TestCase data in inputs){
-                var num01 = data.num01;
-                var num02 = data.num02;
-                int res = 0;
-                
-                //res에 num01과 num02 중에서 더 큰 값을 저장해주세요.(int 형)
-                //여기에 코드를 작성해주세요.
-                res = (num01 > num02) ? num01 : num02;
-                results.Add(res);
+        public object main(object data){
+            string inputData = ((TestCase01.TestCase)data).inputData; // inputData
+            //res에 값을 저장시켜주세요.
+            Toy[] res = {}; // 파싱하고 값을 바르게 넣기
+            List<Toy> toys = new List<Toy>();
+            string[] parse_data = inputData.Split('\n');
+            foreach(var tmp in parse_data){
+                var toy = new Toy(tmp);
+                toys.Add(toy);
             }
+            res = toys.ToArray();
 
-            output(results.ToArray());
+            //return 값은 Toy Class의 Array 형식으로 Toy[] 입니다.
+            return res;
         }
     }
 }
